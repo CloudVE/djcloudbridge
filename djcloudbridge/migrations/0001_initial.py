56ddb4e749734e11e38628bfb3f2a6b8abe6ae8e
almahmoud <amahmou4@jhu.edu>
2019-02-25 12:23:22 -0500

    Migration for GCP rename

--- djcloudbridge/migrations/0001_initial.py
+++ djcloudbridge/migrations/0001_initial.py
@@ -146,27 +146,27 @@ class Migration(migrations.Migration):
             bases=('djcloudbridge.credentials',),
         ),
         migrations.CreateModel(
-            name='GCP',
+            name='GCE',
             fields=[
                 ('cloud_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='djcloudbridge.Cloud')),
                 ('region_name', models.CharField(max_length=100)),
                 ('zone_name', models.CharField(max_length=100)),
             ],
             options={
-                'verbose_name': 'GCP',
-                'verbose_name_plural': 'GCP',
+                'verbose_name': 'GCE',
+                'verbose_name_plural': 'GCE',
             },
             bases=('djcloudbridge.cloud',),
         ),
         migrations.CreateModel(
-            name='GCPCredentials',
+            name='GCECredentials',
             fields=[
                 ('credentials_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='djcloudbridge.Credentials')),
                 ('credentials', fernet_fields.fields.EncryptedTextField()),
             ],
             options={
-                'verbose_name': 'GCP Credential',
-                'verbose_name_plural': 'GCP Credentials',
+                'verbose_name': 'GCE Credential',
+                'verbose_name_plural': 'GCE Credentials',
             },
             bases=('djcloudbridge.credentials',),
         ),
